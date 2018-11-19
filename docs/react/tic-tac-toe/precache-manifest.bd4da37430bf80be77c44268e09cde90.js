7fee5840d5defc4e79830f1d25270fcbc161194c
Al-un <alun.sng@gmail.com>
2018-11-19 20:07:58 +0100

    Adding docs folder

---
+++ docs/react/tic-tac-toe/precache-manifest.bd4da37430bf80be77c44268e09cde90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "098147ff3d7bf8754a91",
+    "url": "/tutorials/react/tic-tac-toe/static/css/main.5d472676.chunk.css"
+  },
+  {
+    "revision": "098147ff3d7bf8754a91",
+    "url": "/tutorials/react/tic-tac-toe/static/js/main.098147ff.chunk.js"
+  },
+  {
+    "revision": "07861e9d5e216d6b0c3e",
+    "url": "/tutorials/react/tic-tac-toe/static/js/1.07861e9d.chunk.js"
+  },
+  {
+    "revision": "602a6cdba8e56e8e9025",
+    "url": "/tutorials/react/tic-tac-toe/static/js/runtime~main.602a6cdb.js"
+  },
+  {
+    "revision": "4411f14da0b5d9bc871f10c89f620ab5",
+    "url": "/tutorials/react/tic-tac-toe/index.html"
+  }
+];
