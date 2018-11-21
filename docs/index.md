51a6e17a4178b9a599683dd38e0ad23a709f296c
Al-un <alun.sng@gmail.com>
2018-11-21 17:08:31 +0100

    publishing React randomuser

--- docs/index.md
+++ docs/index.md
@@ -4,9 +4,11 @@ Here are some deployed tutorial applications:
 
 ## React
 
-0.  [Test-project](/tutorials/react/test-project) from 
+1.  [Test-project](/tutorials/react/test-project) from
     [this tutorial](https://scotch.io/tutorials/learning-react-getting-started-and-concepts)
-1.  [Tic-tac-toe](/tutorials/react/tic-tac-toe) from 
+2.  [Tic-tac-toe](/tutorials/react/tic-tac-toe) from
     [React tutorial](https://reactjs.org/tutorial/tutorial.html)
-2.  [Main concepts](/tutorials/react/main-concepts) from 
+3.  [Main concepts](/tutorials/react/main-concepts) from
     [React main concepts](https://reactjs.org/docs/hello-world.html)
+4.  [Randomuser](/tutorials/react/randomuser) from repository
+    (https://github.com/Al-un/tutorials/tree/master/react/3.randomuser)
---
+++ docs/react/randomuser/precache-manifest.978967f47f25bb48f2db00d14176c667.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a03e6112e1dffb3437c",
+    "url": "/tuturials/react/randomuser/static/css/main.e306eae8.chunk.css"
+  },
+  {
+    "revision": "0a03e6112e1dffb3437c",
+    "url": "/tuturials/react/randomuser/static/js/main.0a03e611.chunk.js"
+  },
+  {
+    "revision": "d855c10c80aa95827f2a",
+    "url": "/tuturials/react/randomuser/static/js/1.d855c10c.chunk.js"
+  },
+  {
+    "revision": "41d87435e941b2af5e26",
+    "url": "/tuturials/react/randomuser/static/js/runtime~main.41d87435.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tuturials/react/randomuser/static/media/react_logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f765dd9f08caa4f6bf74d239be05c4ed",
+    "url": "/tuturials/react/randomuser/index.html"
+  }
+];
