7e11f68a9180a1612b6637da52166e89f00ecc4b
Al-un <alun.sng@gmail.com>
2018-12-25 22:55:48 +0100

    Webpack - development

---
+++ js_webpack/03.development/src/index.js
@@ -0,0 +1,4 @@
+import _ from 'lodash';
+import component from './component';
+
+document.body.appendChild(component());
---
+++ js_webpack/03.development/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output management /w plugin',
+      template: path.resolve(__dirname, 'public', 'index.html'),
+      filename: './index.html'
+    }),
+    new CleanWebpackPlugin(['dist']),
+    new ManifestPlugin()
+  ]
+};
