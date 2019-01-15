bb6d1a283cc391bb55c429634e90df29306501b2
Al-un <alun.sng@gmail.com>
2019-01-15 13:21:26 +0100

    adding other components

--- angular/07.arsprojects/projects/ars-components/src/lib/ars-components.module.ts
+++ angular/07.arsprojects/projects/ars-components/src/lib/ars-components.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { ArsComponentsComponent } from './ars-components.component';
+import { CheckboxComponent } from './checkbox/checkbox.component';
+import { TextinputComponent } from './textinput/textinput.component';
+import { PouetComponent } from './pouet/pouet.component';
+import { PlopComponent } from './plop/plop.component';
 
 @NgModule({
-  imports: [
-  ],
-  declarations: [ArsComponentsComponent],
-  exports: [ArsComponentsComponent]
+  imports: [],
+  declarations: [ArsComponentsComponent, CheckboxComponent, TextinputComponent, PouetComponent, PlopComponent],
+  exports: [ArsComponentsComponent, CheckboxComponent, TextinputComponent, PouetComponent, PlopComponent]
 })
-export class ArsComponentsModule { }
+export class ArsComponentsModule {}
