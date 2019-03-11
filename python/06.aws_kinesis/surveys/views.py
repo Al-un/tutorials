21d3c7133144df15da22b79b7a930a41dbb3f77c
Al-un <alun.sng@gmail.com>
2019-03-11 11:24:23 +0100

    Testing Kinesis sync or async

--- python/06.aws_kinesis/surveys/views.py
+++ python/06.aws_kinesis/surveys/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from datetime import datetime
 
 from .models import Survey, Choice
 from .kinesis import put_vote_to_stream
@@ -12,10 +13,13 @@ class SurveyView(generic.ListView):
 
 
 def vote(request, choice_id):
+    print("%s Voting for %s" % (datetime.now, choice_id))
     choice = get_object_or_404(Choice, pk=choice_id)
     choice.count += 1
     choice.save()
 
+    print("%s Prepare for sending to Kinesis from view" % datetime.now)
     put_vote_to_stream(request, choice)
+    print("%s Kinesis finished from view" % datetime.now)
 
     return HttpResponseRedirect(reverse('surveys:index'))
