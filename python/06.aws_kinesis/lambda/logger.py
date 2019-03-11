bedbc790b451ea9106d388ba02025d46b3f9a703
Al-un <alun.sng@gmail.com>
2019-03-11 10:39:14 +0100

    Testing kinesis sync + mig to eu-west-3

--- python/06.aws_kinesis/lambda/logger.py
+++ python/06.aws_kinesis/lambda/logger.py
@@ -2,8 +2,9 @@
 import json
 import psycopg2
 import base64
+import time
 
-CONN_STRING = "user='pouet' password='PlopPlop12' host='learn-aws-kinesis.crbos0rlicqy.ap-northeast-1.redshift.amazonaws.com' port='5439' dbname='dev'"
+CONN_STRING = "user='pouet' password='PlopPlop12' host='learn-aws-kinesis.cm7tssjh5ymm.eu-west-3.redshift.amazonaws.com' port='5439' dbname='dev'"
 
 def lambda_handler(event, context):
 
@@ -13,6 +14,9 @@ def lambda_handler(event, context):
     conn = psycopg2.connect(CONN_STRING)
     cursor = conn.cursor()
     sql_temp = "INSERT INTO votes (ip, choice_id) VALUES (%s, %s)"
+
+    # pseudo wait 5 seconds
+    time.sleep(2)
 
     # Decrypt payload
     # https://docs.aws.amazon.com/lambda/latest/dg/with-kinesis-create-package.html#with-kinesis-example-deployment-pkg-python
--- python/06.aws_kinesis/surveys/kinesis.py
+++ python/06.aws_kinesis/surveys/kinesis.py
@@ -1,10 +1,12 @@
 import boto3
 from django.conf import settings
 import json
+from datetime import datetime
 
 kinesis_stream = 'learn-aws-kinesis'
 kinesis_client = boto3.client('kinesis',
-                              region_name='ap-northeast-1',
+                            #   region_name='ap-northeast-1',
+                              region_name='eu-west-3',
                               aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
                               aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
 
@@ -17,6 +19,9 @@ def put_vote_to_stream(request, choice):
     }
     payload_json = json.dumps(payload)
 
+    now = datetime.now()
+    print("%s prepare for kinensis_client.put_record" % now)
     output = kinesis_client.put_record(
         StreamName=kinesis_stream, Data=payload_json, PartitionKey='vote')
+    print("%s kinesis_client.put_record finished" % now)
     print(output)
